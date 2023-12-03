d0b4ff39014e4e0e54c800172471e3be7949602f
v0vabdemort <mamyshev.vladimir1@yandex.ru>
2023-12-03 17:02:59 +0700

    v.0,9 - registration,login is active, func change personData is active (bug - birthday)

--- app/backend/php/userPage.php
+++ app/backend/php/userPage.php
@@ -144,8 +144,7 @@ if(isset($_SESSION['logged-in'])) {
 
 
 
-<script src="public/js/menu.js"></script>
-<script src="public/js/checkPassRegistr.js"></script>
+<script src="../../public/js/checkPassRegistr.js"></script>
 <script src = "https://smartcaptcha.yandexcloud.net/captcha.js?render=onload&onload=onSmartCaptchaReady"
         defer></script>
 </body>
